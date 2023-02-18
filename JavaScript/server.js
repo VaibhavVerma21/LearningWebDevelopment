99c31679de09c124179de79a9ff1a05dc835114c
Vaibhav <vaibhavverma.2106@gmail.com>
2023-02-18 20:56:40 +0530

    13 Feb 23 'JavaScript Basics - Printing, Data Types, Array Declaration, Variable and Constants, Operators'

---
+++ JavaScript/server.js
@@ -0,0 +1,55 @@
+// Started Video 13 Feb 2023
+{
+    // This is a comment
+
+    // Normal Print Statment
+    console.log("Vaibhav Verma"); 
+    console.log(this)
+
+    // Data Types
+    let name = "Vaibhav Verma"; // String Value
+    let age = 16; // Integer Value
+    let decimal = 0.667; // Float Value 
+    let boolean = false; // Boolean Value
+    let null_ = null; // Null Value
+    let undefined_ = undefined; // Undefined Value
+
+    // Arrays
+    let stuff = ["Vaibhav Verma", 16, 0.667, false, null, undefined]; // Array of previous stuff
+
+    // Variables and Constants
+    let variable = 10; // Declares and assigns variables a value
+    variable = 20; // Value can be changed, will not throw exception
+    const constant = 10; // Declares and assigns variables a value
+    //variable = 20; // Value can not be changed, will throw exception
+
+    // Printing variables
+    let cost = 3500;
+    console.log(cost);
+    cost = 2000;
+    console.log(cost);
+
+    // Operators
+    let assignment = 1; // Assignment Operator
+    let addition = 2+1; // Addition Operator
+    let subtraction = 2-1; // Subtraction Operator
+    let multiplication = 2*1; // Multiplication Operator
+    let division = 2/1; // Division Operator
+    let modulus = 5%2; // Modulus Operator
+    let power = 2**3; // Power Operator
+
+    // Comparison Operators
+    let greaterThan = 3>2; // Greater Than Operator
+    let smallerThan = 2<3; // Smaller Than Operator
+    let greaterThanOrEqualTo = 3>=2; // Greater Than Or Equal To Operator
+    let smallerThanOrEqualTo = 2<=3; // Smaller Than Or Equal To Operator
+    let equalTo = 3=="3"; // Equal To Operator (Ignores data type)
+    let strictEqualTo = 3=="3"; // Strict Equal To Operator (Doesn't Ignore data type)
+    let notEqualTo = 3!="3"; // Not Equal To Operator
+
+    // Logical Operators
+    let and = true && true; // And Operator
+    let or = false || true; // Or Operator
+    let not = !false; // Not Operator
+}
+// Finished Video 13 Feb 2023
